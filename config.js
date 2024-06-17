57a7b0ddb42321b7741e91345cc43f6883550f8f
KiyotakaX011 <142149939+KiyotakaX011@users.noreply.github.com>
2024-06-17 14:44:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,36 +16,36 @@ global.autoreadgc = process.env.AUTOREADGC || true;
 
 
 // Auth information
-global.pairNumber = "919002162806";                         // Add your paining number with country code example "916297175943"; 
+global.pairNumber = "918767585726";                         // Add your paining number with country code example "916297175943"; 
 global.port = process.env.PORT || "10000";
 global.auth = process.env.AUTH || "Pairing";                // Auth mode OR/Pairing.
 global.sessionFile = process.env.SESSION_FILE || "A17-SESSION";
-global.mongodb = process.env.MONGODB || "";                 // Mongodb url.
+global.mongodb = process.env.MONGODB || "mongodb+srv://Rohit:<password>@cluster0.i2z06nv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";                 // Mongodb url.
 global.website = "https://www.exenoz.tech"; 
-global.github = "https://github.com/Kai0071";
+global.github = "https://github.com/NehaSama4833";
 
 
 // Default prefix
-global.prefa = process.env.PREFIX ? process.env.PREFIX.split(",") : ["."];
+global.prefa = process.env.PREFIX ? process.env.PREFIX.split(",") : ["!"];
 
 
 // Owner information
-global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["916297175943", "919362639362"];
-global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["916297175943", "919362639362"];
-global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["916297175943"];
-global.OwnerName = process.env.OWNER_NAME || "Kai";
-global.BotName = process.env.BOT_NAME || "A17";
-global.packname = process.env.PACK_NAME || "A17 Bot";
-global.author = "By: Kai";
-global.BotSourceCode = "https://github.com/Kai0071/A17";
-global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["Neha_Sama"];
+global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["918767585726"];
+global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["918767585726"];
+global.OwnerName = process.env.OWNER_NAME || "Neha";
+global.BotName = process.env.BOT_NAME || "Natsu-MD";
+global.packname = process.env.PACK_NAME || "Natsu Bot";
+global.author = "By: Neha Sama";
+global.BotSourceCode = "https://github.com/NehaSama4833/A17";
+global.SupportGroupLink = "-";
 
 
 //
 global.openAiAPI = process.env.OPENAI_API || "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";
 global.location = process.env.LOCATION || "West Bengal, India";
-global.reactmoji = process.env.REACT_MOJI || "❤️";
-global.themeemoji = process.env.THEME_EMOJI || "💖";
+global.reactmoji = process.env.REACT_MOJI || "🐸";
+global.themeemoji = process.env.THEME_EMOJI || "👾";
 global.vidmenu = { url: process.env.VID_MENU_URL || 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' };
 
 
